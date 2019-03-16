f2628798cf7243a4dc9bc02bf0aae0172121298c
Andrew Bril <andrew.bril@mail.ru>
2019-03-16 21:53:59 +0300

    feat: add love triangle function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  preferences.unshift('');
+  let blackList = [];
+  let count = 0;
+
+  for(let i = 1; i <= preferences.length; i++) {
+    if(blackList.includes(preferences[i]) || i == preferences[i]) continue;
+    if(i == preferences[preferences[preferences[i]]]) { 
+      count++;
+      blackList.push(preferences[i],preferences[preferences[i]],preferences[preferences[preferences[i]]]);
+    }
+  }
+  return count;
+
 };
